34f5a9f6ae5242ff72d6e1467a9d35e8829057aa
Mischa <mischavandenburg@gmail.com>
2022-10-06 20:04:52 +0200

    day 1 completed

---
+++ 1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Hello! Welcome to the band generator.")
+city = input("What city did you grow up in?\n")
+pet = input("What is your favourite pet's name?\n")
+print(f"Your band name is: {city} {pet}")
