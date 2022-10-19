a4c3bd51702532d808b6b814548ece019a1cebca
Mischa <mischavandenburg@gmail.com>
2022-10-19 10:19:07 +0200

    added comments to day 14

--- 14_higher_lower/higher_lower.py
+++ 14_higher_lower/higher_lower.py
@@ -7,6 +7,9 @@ score = 0
 
 
 def get_winner(dictionary_a, dictionary_b):
+    """
+    Returns the winner and the winner's data
+    """
     if dictionary_a['follower_count'] > dictionary_b['follower_count']:
         return ["a", dictionary_a]
     elif dictionary_b['follower_count'] > dictionary_a['follower_count']:
@@ -25,15 +28,17 @@ def game(person_a, person_b, score):
 
     winner = get_winner(person_a, person_b)
 
+    # if guess is correct, start a new round by calling the function again
     if choice == winner[0]:
         os.system("clear")
         score += 1
         game(winner[1], random.choice(data), score)
-    else:
-        os.system("clear")
-        print(logo)
-        print("You lose.")
-        print(f"Your final score: {score}")
+
+    # if the guess is wrong, end the game
+    os.system("clear")
+    print(logo)
+    print("You lose.")
+    print(f"Your final score: {score}")
 
 
 game(random.choice(data), random.choice(data), score)
